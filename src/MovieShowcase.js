33ce701a913850cd6f89c385eaa986c216a75734
Jade Lynn Masker <jadelynn@masker.family>
2021-02-20 18:56:13 -0500

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -4,8 +4,16 @@ import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
 
-  generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
+  generateMovieCards() {
+    return movieData.map((movie, index) => {
+      return <MovieCard
+        key={index}
+        title={movie.title}
+        IMDBRating={movie.IMDBRating}
+        genres={movie.genres}
+        poster={movie.poster}
+      />
+    })
   }
 
   render() {
